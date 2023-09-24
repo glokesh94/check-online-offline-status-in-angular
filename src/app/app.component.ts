a1726991a2789d277ba15bcdd7696e9f7771c55c
glokesh94 <glokesh94@gmail.com>
2023-09-24 19:31:17 +0530

    connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component, ViewChild, ElementRef } from "@angular/core";
+import { Component, OnDestroy, OnInit, VERSION } from '@angular/core';
+import { fromEvent, merge, of, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  name = "Angular Html To Pdf ";
+export class AppComponent implements OnInit, OnDestroy {
+  internetStatus: boolean = false;
+  internetStatus$: Subscription = Subscription.EMPTY;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.checkInternetStatus();
+  }
+
+  ngOnDestroy(): void {
+    this.internetStatus$.unsubscribe();
+  }
+
+  checkInternetStatus() {
+    this.internetStatus = navigator.onLine;
+    this.internetStatus$ = merge(
+      of(null),
+      fromEvent(window, 'online'),
+      fromEvent(window, 'offline')
+    )
+      .pipe(map(() => navigator.onLine))
+      .subscribe(status => {
+        console.log('status', status);
+        this.internetStatus = status;
+      });
+  }
 }
