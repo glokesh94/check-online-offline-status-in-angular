9f63c63b0fafdfefb7403b6e08b14c20f60a6cf3
glokesh94 <33577215+glokesh94@users.noreply.github.com>
2023-09-24 17:18:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# check-online-offline-status-in-angular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/check-online-offline-status-in-angular)
---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnDestroy, OnInit, VERSION } from '@angular/core';
+
+import { fromEvent, merge, of, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  networkStatus: any;
+  networkStatus$: Subscription = Subscription.EMPTY;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.checkNetworkStatus();
+  }
+
+  ngOnDestroy(): void {
+    this.networkStatus$.unsubscribe();
+  }
+
+  checkNetworkStatus() {
+    this.networkStatus = navigator.onLine;
+    this.networkStatus$ = merge(
+      of(null),
+      fromEvent(window, 'online'),
+      fromEvent(window, 'offline')
+    )
+      .pipe(map(() => navigator.onLine))
+      .subscribe(status => {
+        console.log('status', status);
+        this.networkStatus = status;
+      });
+  }
+}
